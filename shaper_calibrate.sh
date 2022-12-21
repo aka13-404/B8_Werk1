938741ef7e429600fd862f02abdee1bd30fa0a77
Alexander Krasnov <krasnov.alx@gmail.com>
2022-12-21 23:08:08 +0100

    add csv processing

---
+++ shaper_calibrate.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+OUTPUT_FOLDER=logs/resonances
+PRINTER_DATA=home/trident/printer_data
+KLIPPER_SCRIPTS_LOCATION=~/klipper/scripts
+RESONANCE_CSV_LOCATION=tmp
+
+if [ ! -d  /$PRINTER_DATA/$OUTPUT_FOLDER/ ] #Check if we have an output folder
+then
+    mkdir /$PRINTER_DATA/$OUTPUT_FOLDER/
+fi
+
+cd /$RESONANCE_CSV_LOCATION/
+
+shopt -s nullglob
+set -- resonances*.csv  
+
+if [ "$#" -gt 0 ]
+then
+    for each_file in resonances*.csv
+    do
+        $KLIPPER_SCRIPTS_LOCATION/calibrate_shaper.py $each_file -o /$PRINTER_DATA/$OUTPUT_FOLDER/${each_file%.*}.png
+        rm /$RESONANCE_CSV_LOCATION/$each_file
+    done
+else
+    echo "Something went wrong, no csv found to process"
+fi
